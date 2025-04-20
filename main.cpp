0af3ba6bbd005439659cb77f5dd09eb55dd33577
Evening Doggie <04.sunrise_picnics@icloud.com>
2025-04-21 01:04:44 +0300

    Move .cpp to .h. Trie test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "trie.h"
+#include <string>
 
 int main()
 {
+    std::string const alphabet = "abcdefghijklmnopqrsty";
+    auto * t = new trie<int>(alphabet);
+    t->upsert("", 100);
+    t->upsert("key", 5);
+    t->upsert("keya", 5);
+    t->upsert("keyb", 6);
+    t->upsert("kkek", 7);
+    t->upsert("kkea", 8);
+    t->upsert("keyba", 9);
+    t->upsert("keybc", 10);
+    t->dispose("keybc");
+    int searched = t->obtain("keyba");
+   std::cout << searched << std::endl;
     return 0;
 }
